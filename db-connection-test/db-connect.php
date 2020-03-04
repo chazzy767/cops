b8d3328f8bea6ecbd375967913cfa617771ae990
chazzy767 <22896112+chazzy767@users.noreply.github.com>
2020-03-03 19:00:27 -0500

    Working connection file

--- db-connection-test/db-connect.php
+++ db-connection-test/db-connect.php
@@ -1,26 +1,13 @@
 <?php
-//mascots.php
-// Change username and password to your MySQL account username and password
-$server = "localhost";
-$userName = "cmczerny";
-$pass = "RZNUq8J2wvaXxKtQ";
+$servername = "localhost";
+$username = "cmczerny";
+$password = "RZNUq8J2wvaXxKtQ";
 $db = "cmczerny";
-//create connection
-$con=mysqli_connect($server,$userName,$pass,$db);
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$db);
 // Check connection
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
 }
-//example insert statement to add a new mascot
-mysqli_query($con,"INSERT INTO tbl_ouMascots(name, YearCreated)
-VALUES (’Grizz’, 1998)");
-//example SELECT stament to show the results of the mascots table
-$result = mysqli_query($con,"SELECT * FROM ’tbl_ouMascots’ WHERE 1 LIMIT 0, 30; ");
-While($row = mysqli_fetch_array($result))
-{
-echo $row[’name’] . " " . $row[’YearCreated’];
-echo "<br>";
-}
-mysqli_close($con);
+echo "Connected successfully";
 ?>
