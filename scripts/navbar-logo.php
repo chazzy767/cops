dca4e1b343538d15bb2f65eccd839824e40d07af
chazzy767 <22896112+chazzy767@users.noreply.github.com>
2020-03-04 01:02:24 -0500

    restructuring

---
+++ scripts/navbar-logo.php
@@ -0,0 +1,14 @@
+<?php
+ echo '<div class="container">
+			<img src="images/logo.jpg" class="logo">
+			<button class="login" onclick="location.href = 'pages/loginPG.php';" type="button"> Login</button>
+			<br>
+			<button class="Signup" onclick="location.href = 'pages/signupPG.php';" type="button"> Sign Up</button>
+		</div>	
+			<ul>
+			  <li><a href="default.asp">Home</a></li>
+			  <li><a href="news.asp">Local News</a></li>
+			  <li><a href="contact.asp">Contact</a></li>
+			  <li><a href="about.asp">About</a></li>
+			</ul>';
+?>
