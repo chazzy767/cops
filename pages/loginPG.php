08ecbecafd40bd9b577da72975f0ddc8abfb43b5
ShelbyLinnette <47544225+ShelbyLinnette@users.noreply.github.com>
2020-03-05 15:40:35 -0500

    Update loginPG.php

--- pages/loginPG.php
+++ pages/loginPG.php
@@ -8,17 +8,17 @@
 	<?php include("../scripts/navbar-log-sub-login.php");?>
 	<br>
   <div class="container">
- <div class="back">
-    <input class="username" type="text" placeholder="Username" name="uname" required>
-	<br>
-    <input class="password" type="password" placeholder="Password" name="psw" required>
-	<br>
-    <button class= "login1" type="button" onclick="location.href = '../html/customerUI.html';" >Login</button>
-	<br>
-    <label class="remember" >
-      <input class="remember" type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-    <div class="psw">Forgot <a href="#">password?</a></div>
+	<div class="back">
+		<input class="username" type="text" placeholder="Username" name="uname" required>
+		<br>
+		<input class="password" type="password" placeholder="Password" name="psw" required>
+		<br>
+		<button class= "login1" type="button" onclick="location.href = '../html/customerUI.html';" >Login</button>
+		<br>
+		<label class="remember" >
+		  <input class="remember" type="checkbox" checked="checked" name="remember"> Remember me
+		</label>
+		<div class="psw">Forgot <a href="#">password?</a></div>
     </div>
 	</div>
 	</form>
