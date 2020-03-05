3d7dc46e46d8f586fe02d06e0cb05b873295ee97
ShelbyLinnette <47544225+ShelbyLinnette@users.noreply.github.com>
2020-03-05 12:47:42 -0500

    newlogin

--- pages/loginPG.php
+++ pages/loginPG.php
@@ -5,26 +5,22 @@
 	</head>
 	<body>
 	<form action="login.php" method="post">
-	<?php include("../scripts/navbar-log-sub.php");?>
+	<?php include("../scripts/navbar-log-sub-login.php");?>
 	<br>
   <div class="container">
-    <label for="uname" style="text-align:center;"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
+ <div class="back">
+    <input class="username" type="text" placeholder="Username" name="uname" required>
 	<br>
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+    <input class="password" type="password" placeholder="Password" name="psw" required>
 	<br>
-    <button type="button" onclick="location.href = '../html/customerUI.html';" >Login</button>
+    <button class= "login1" type="button" onclick="location.href = '../html/customerUI.html';" >Login</button>
 	<br>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
+    <label class="remember" >
+      <input class="remember" type="checkbox" checked="checked" name="remember"> Remember me
     </label>
-  </div>
-
-  <div class="container" >
- 
     <div class="psw">Forgot <a href="#">password?</a></div>
-  </div>
+    </div>
+	</div>
 	</form>
 	</body>
 </html>
