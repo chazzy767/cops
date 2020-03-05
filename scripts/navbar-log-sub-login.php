34c52b6a21f07673f784717788cecb68da1e7e51
ShelbyLinnette <47544225+ShelbyLinnette@users.noreply.github.com>
2020-03-05 16:07:55 -0500

    2

--- scripts/navbar-log-sub-login.php
+++ scripts/navbar-log-sub-login.php
@@ -2,9 +2,7 @@
  //This PHP file generates the navbar for subpages only!!! due to relative paths
  echo '<div class="container">
 			<img src="../images/logo.jpg" class="logo">
-			<button class="login" onclick="location.href = \'../pages/loginPG.php\';" type="button"> Login</button>
-			<br>
-			<button class="Signup" onclick="location.href = \'../pages/signupPG.php\';" type="button"> Sign Up</button>
+		
 		</div>	
 			';
 ?>
--- scripts/navbar-logo.php
+++ scripts/navbar-logo.php
@@ -1,17 +1,19 @@
 <?php
 //This PHP file generates the navbar for the index page only!!! due to relative paths
- echo '<div class="container">
+ echo ' <ul>
+		  <li><a href="index.php">Home</a></li>
+		  <li><a href="">Local News</a></li>
+		  <li><a href="">Contact</a></li>
+		  <li><a href="">About</a></li>
+		</ul>
+		<div class="container">
+	
 			<img src="images/logo.jpg" class="logo">
-			<button class="login" onclick="location.href = \'pages/loginPG.php\';" type="button"> Login</button>
+			<button class="login" onclick="location.href = \'pages/loginPG.php\';" type="button"> User Login</button>
 			<br>
 			<button class="Signup" onclick="location.href = \'pages/signupPG.php\';" type="button"> Sign Up</button>
 			<br>
-			<button class="Admin" onclick="location.href = \'assetes/html/adminUI.php\';" type="button"> Admin Login</button>
+			<button class="admin" onclick="location.href = \'assetes/html/adminUI.php\';" type="button"> Admin Login</button>
 		</div>	
-			<ul>
-			  <li><a href="index.php">Home</a></li>
-			  <li><a href="">Local News</a></li>
-			  <li><a href="">Contact</a></li>
-			  <li><a href="">About</a></li>
-			</ul>';
+			';
 ?>
