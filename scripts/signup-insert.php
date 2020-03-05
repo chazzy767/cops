d91e75ea03e2499b177a1618fce63c1f21c6c5a2
chazzy767 <22896112+chazzy767@users.noreply.github.com>
2020-03-05 02:05:29 -0500

    Redirections
    Redirection from form submission added. Actual insert code not working.

--- scripts/signup-insert.php
+++ scripts/signup-insert.php
@@ -1,6 +1,16 @@
 <?php
  //connects to our OU COPS db
  include("../scripts/connect-db.php");
+ echo "<br>";
+ echo "Inserting data...";
+ echo "<br>";
  
- 
+ $sql = "INSERT INTO Homeowner (ho_fname, ho_lname, ho_address,
+ ho_email, ho_username, ho_city, ho_state, ho_zip)
+ VALUES
+ ('$_POST[fname]', '$_POST[lname]', '$_POST[adress]', '$_POST[email]', 
+ '$_POST[uname]', '$_POST[city]', '$_POST[state]', '$_POST[zip]')";
+ echo "Closing connection and redirecting...";
+ header("Refresh:2, url=http://www.secs.oakland.edu/~cmczerny/cops/", true, 303);
+ exit ;
 ?>
