cb7724289639e5519980fd181a767d9442137671
chazzy767 <22896112+chazzy767@users.noreply.github.com>
2020-03-05 10:54:36 -0500

    Fix for links on subpages

--- scripts/navbar-log-sub.php
+++ scripts/navbar-log-sub.php
@@ -2,9 +2,9 @@
  //This PHP file generates the navbar for subpages only!!! due to relative paths
  echo '<div class="container">
 			<img src="../images/logo.jpg" class="logo">
-			<button class="login" onclick="location.href = \'pages/loginPG.php\';" type="button"> Login</button>
+			<button class="login" onclick="location.href = \'../pages/loginPG.php\';" type="button"> Login</button>
 			<br>
-			<button class="Signup" onclick="location.href = \'pages/signupPG.php\';" type="button"> Sign Up</button>
+			<button class="Signup" onclick="location.href = \'../pages/signupPG.php\';" type="button"> Sign Up</button>
 		</div>	
 			<ul>
 			  <li><a href="default.asp">Home</a></li>
