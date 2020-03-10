93c834cc64869173c6870a1decb3fbb59917ff61
sbouch <46934530+sbouch@users.noreply.github.com>
2020-03-10 12:13:31 -0400

    Update navbar-logo.php

--- scripts/navbar-logo.php
+++ scripts/navbar-logo.php
@@ -2,7 +2,7 @@
 //This PHP file generates the navbar for the index page only!!! due to relative paths
  echo ' <ul>
 		  <li><a href="index.php">Home</a></li>
-		  <li><a href="">Local News</a></li>
+		  <li><a href="">News</a></li>
 		  <li><a href="">Contact</a></li>
 		  <li><a href="">About</a></li>
 		</ul>
@@ -13,11 +13,7 @@
 			<br>
 			<button class="Signup" onclick="location.href = \'pages/signupPG.php\';" type="button"> Sign Up</button>
 			<br>
-<<<<<<< HEAD
 			<button class="Admin" onclick="location.href = \'pages/adminUI.php\';" type="button"> Admin Login</button>
-=======
-			<button class="admin" onclick="location.href = \'assetes/html/adminUI.php\';" type="button"> Admin Login</button>
->>>>>>> 1c2b1948b0a6499abfa609c4d6caa6941b2fb8a4
 		</div>	
 			';
 ?>
