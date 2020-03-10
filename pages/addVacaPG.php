651f336dd49ddc3493e24df7ab28caddae1cfbe1
ShelbyLinnette <47544225+ShelbyLinnette@users.noreply.github.com>
2020-03-10 12:58:02 -0400

    5

---
+++ pages/addVacaPG.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include("../scripts/sub-head.php"); ?>
+	</head>
+	<body>
+	<form action="login.php" method="post">
+	<?php include("../scripts/navbar-log-sub-login.php");?>
+	<br>
+  <div class="container">
+	<div class="add">
+	add vacation
+	</div>
+	</div>
+	</form>
+	</body>
+</html>
---
+++ pages/contactPG.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Contact</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="../css/stylesheet.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	</head>
+	<body>
+			<ul>
+			  <li><a href="default.asp">Home</a></li>
+			  <li><a href="news.asp">Local News</a></li>
+			  <li><a href="contact.asp">Contact</a></li>
+			  <li><a href="about.asp">About</a></li>
+			</ul>
+			<div class="container"> 
+				<img src="../images/logo.JPG" class="logo">
+				<button class="Signup" onclick="location.href = 'assetes/html/profileEdit.html';" type="button"> Profile</button>
+				<button class="login" onclick="location.href = '../../homePG.html';" type="button"> Log Off</button>
+				<br>
+			</div> 
+			<div class="container">
+			<div class="meet">
+				<p4> Meet the Team! </p4> <br><br>
+				<p5> Shelby Smith </p5> <br> <p7> Team Lead and Project Designer </p7> <br>  <p8>  shelbysmith@oakland.edu</p8> <br> <p6> Shelby Smith is a
+				4th year senior at Oakland University pursuing a degree in Information 
+				Technology. Shelby has experience working with front end coding in previous 
+				projects and direct contact with the police department. Shelby is working
+				with the police department to get all of the requirements for the project, 
+				to create a plan for the team to execute.  </p6><br><br>
+				
+				<p5> Ian Ray </p5> <br> <p7> Programmer/Implementation </p7> <br> <p8> imray@oakland.edu</p8> <br> <p6> Ian Ray is a 
+				senior at Oakland University and is pursuing a degree in Information Technology. Ian has 
+				worked on apps that include everything from front end coding to back-end databases. Ian has 
+				designed the user interface and will work towards implementing as many features necessary to 
+				make the project a success. </p6><br><br>
+				
+				<p5> Sean Boucher </p5> <br> <p7> Server Manager </p7> <br> <p8>  ssboucher@oakland.edu </p8> <br> <p6> Sean is a senior pursuing a degree in Information 
+				Technology at Oakland University. Sean has been a part of many software development teams 
+				with an end goal of creating applications to make end users' lives easier. </p6><br><br>
+				
+				<p5> Chad Czerny </p5> <br> <p7> Database Designer </p7> <br> <p8>  cmczerny@oakland.edu</p8> <br> <p6> Chad is a senior at Oakland pursuing his Bachelor 
+				of Science degree in Information Technology. His interests within the field 
+				include primarily computer networks and system administration. He has worked on 
+				several personal and school projects involving databases, Python, and HTML/CSS/JS. 
+				His role in this project is the database designer and he hopes to use past 
+				experience with databases as well as learn new aspects of database design to 
+				contribute to the success of the project. </p6>
+
+			</div>
+			</div>
+	</body>
+</html> 
