50c69963ed915c3f2659d73d72134296170df0ea
ShelbyLinnette <47544225+ShelbyLinnette@users.noreply.github.com>
2020-03-10 15:16:51 -0400

    Update navbar-logo.php

--- scripts/navbar-logo.php
+++ scripts/navbar-logo.php
@@ -3,8 +3,8 @@
  echo ' <ul>
 		  <li><a href="index.php">Home</a></li>
 		  <li><a href="">News</a></li>
-		  <li><a href="pages/contactPG.php">Contact</a></li>
-		  <li><a href="pages/about.php">About</a></li>
+		  <li><a href="../pages/contactPG.php">Contact</a></li>
+		  <li><a href="../pages/about.php">About</a></li>
 		</ul>
 		<div class="container">
 	
@@ -13,7 +13,7 @@
 			<br>
 			<button class="Signup" onclick="location.href = \'pages/signupPG.php\';" type="button"> Sign Up</button>
 			<br>
-			<button class="Admin" onclick="location.href = \'pages/adminUI.php\';" type="button"> Admin Login</button>
+			
 		</div>	
 			';
 ?>
