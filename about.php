ea483387c3d74b53a3222962ada587c9c31a3293
ianray101010 <61749999+ianray101010@users.noreply.github.com>
2020-03-10 12:34:13 -0400

    Add files via upload

--- about.php
+++ about.php
@@ -12,7 +12,7 @@
 	<p>when you first make your account, you will be promted to enter your personal information and address. once logged in, you can sign up for a house check. this will take you to a form to fill out where you will fill in any information about the house you want the house checker to know about. enter the dates you will be leaving and returning, and then you are free to go on vacation. A house checker will check your house peiodicall while you are away to make sure nothng has happened since you left.</p>
 	<p>once the house check has been submitted, an administrator will log in and see your house check submition among a list of others. the house checker will be able to click on an address where a house check form has been submitted, and a map api of the house will appear for them so they know where it is. the house checker will then proceed to the house and fill out his own form verifiying the information that the reseident origionally submitted. once the house checker has verified that the house is ok and the check is completed, the checker will submit his form and will move on to the next house check. an update to the residents profile will appear showing the time and status of the house checks completion.</p>
 	<p>in the event the house checker notices something has gone wrong, they will try to contact the homeowner and verify that it is ok. if the house checker cannot, they will contact the authorities and inform them that the house has been tampered with.</p>
-<h2>about the creators of the app</h2>
-<p>this is where we put the description of the us</p>
+	<h2>about the creators of the app</h2>
+	<p>this is where we put the description of the us</p>
 	</body>
 </html>
