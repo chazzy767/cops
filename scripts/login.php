926e298b378d7bbf1a014a798101202cb13bd706
chazzy767 <22896112+chazzy767@users.noreply.github.com>
2020-03-21 15:34:34 -0400

    Redirects Based on User Type

--- scripts/login.php
+++ scripts/login.php
@@ -6,22 +6,33 @@
  
  $myusername = $_POST['uname'];
 
- //Selects password from row w/ entered username
- $sql = "SELECT ho_pw_hash FROM Homeowner WHERE ho_username = '{$myusername}'";
+ //Selects password and user type from row w/ entered username
+ $sql = "SELECT ho_pw_hash, user_type FROM Homeowner WHERE ho_username = '{$myusername}'";
  $query = mysqli_query($conn,$sql);
  $result = mysqli_fetch_assoc($query);
  
  //Extracts the actual pw string
  $fetched_pw = $result['ho_pw_hash'];
  $mypassword = crypt($_POST['psw'],$fetched_pw);
+ $user_type = $result['user_type'];
  
+ echo 'User Type: '.$user_type.'<br>';
  //Compares passwords
   if (strcmp($mypassword,$fetched_pw)===0){
 	 echo "Login successful. Redirecting...";
 	 session_start();
 	 $_SESSION["session_username"] = $myusername;
-	 header("Refresh:3, url=http://www.secs.oakland.edu/~cmczerny/cops/pages/customerUI.php",
-	 true, 303);
+	 $_SESSION["user_type"] = $user_type;
+	 if ($user_type == "standard"){
+		 header("Refresh:3, url=http://www.secs.oakland.edu/~cmczerny/cops/pages/customerUI.php",
+	     true, 303);
+	 } elseif ($user_type == "admin"){
+		 header("Refresh:3, url=http://www.secs.oakland.edu/~cmczerny/cops/pages/adminProfile.php",
+	     true, 303);
+	 } else {
+		 header("Refresh:3, url=http://www.secs.oakland.edu/~cmczerny/cops/",
+	     true, 303);
+	 }
  } else{
 	 echo "Login unsuccessful.";
  }
