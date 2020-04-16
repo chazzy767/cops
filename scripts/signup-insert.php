d95bbc4eacd457134a22ca92c47396374a9b9535
chazzy767 <22896112+chazzy767@users.noreply.github.com>
2020-04-16 06:06:13 -0400

    Data Validation/Alert Attempts

--- scripts/signup-insert.php
+++ scripts/signup-insert.php
@@ -11,8 +11,8 @@
  $ho_address = mysqli_real_escape_string($conn, $_POST['adress']);
  $ho_email = mysqli_real_escape_string($conn, $_POST['email']);
  $ho_username = mysqli_real_escape_string($conn, $_POST['uname']);
-	$ho_phone = mysqli_real_escape_string($conn, $_POST['phone']);
-	
+ $ho_phone = mysqli_real_escape_string($conn, $_POST['phone']);
+ 
  //password is hashed
  $ho_pw = mysqli_real_escape_string($conn, crypt($_POST['pword']));
  $ho_city = mysqli_real_escape_string($conn, $_POST['city']);
@@ -21,64 +21,75 @@
  
  $valid_flag = true;
  
-	//Validates email
-	if (!filter_var($ho_email, FILTER_VALIDATE_EMAIL)) {
-			echo "Error: Email is invalid" . "<br>";
-			$valid_flag = false;
-	}
-	
-	//validates phone number
-	if(preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $ho_phone)) {
-  //do nothing
- } else {
-		$valid_flag = false;
-		echo "Error: Phone number is not valid." . "<br>";
-	}
-	
-	//validates zip code
-	if(preg_match("/^([0-9]{5})(-[0-9]{4})?$/",$ho_zip)) {
-  //do nothing
- } else {
-		$valid_flag = false;
-		echo "Error: Zip code is not valid." . "<br>";
-	}
+ //Validates email
+ if (!filter_var($ho_email, FILTER_VALIDATE_EMAIL)) {
+   echo "Error: Email is invalid" . "<br>";
+   $valid_flag = false;
+ }
  
-	//validates username
+ //validates to see if email exists
+ $sql_e = "SELECT *
+           FROM Homeowner
+           WHERE ho_email = '$ho_email'";
+ $res_e = mysqli_query($conn, $sql_e);
+ 
+ if(mysqli_num_rows($res_e) > 0){
+     $valid_flag = false;
+     echo "Error: Email already taken.<br>"; 
+ }
+ 
+ //validates phone number
+ if(preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $ho_phone)) {
+  //do nothing
+ } else {
+  $valid_flag = false;
+  echo "Error: Phone number is not valid." . "<br>";
+ }
+ 
+ //validates zip code
+ if(preg_match("/^([0-9]{5})(-[0-9]{4})?$/",$ho_zip)) {
+  //do nothing
+ } else {
+  $valid_flag = false;
+  echo "Error: Zip code is not valid." . "<br>";
+ }
+ 
+ //validates username
  $sql_u = "SELECT * FROM Homeowner WHERE ho_username='$ho_username'";
-	$res_u = mysqli_query($conn,$sql_u);
-	
-	if (mysqli_num_rows($res_u) > 0){
-		$valid_flag = false;
-		echo "Error: Username is taken" . "<br>";
-	} else {
-		//do nothing
-	}
-			
-	
-	//if there are no errors, then insert data
-	if ($valid_flag == true){
-		//actual SQL code to put in our DB tables
-		$sql = "INSERT INTO Homeowner (ho_fname,
-		ho_lname,ho_address,ho_email,ho_username,ho_pw_hash,
-		ho_city,ho_state,ho_zip) VALUES ('$ho_fname',
-		'$ho_lname','$ho_address','$ho_email','$ho_username',
-		'$ho_pw','$ho_city','$ho_state','$ho_zip')";
-		
-		//checks to see if query was executed/inserted
-		if(!mysqli_query($conn,$sql)){
-			//echo "Account info not inserted";
-		} else { //else if query is executed, the following runs
-			//echo "<br>";
-			//echo "Account info inserted";
-			//echo "<br>";
-			
-		}
-		mysqli_close($conn);
-		header("Location: ../index.php",
+ $res_u = mysqli_query($conn,$sql_u);
+ 
+ if (mysqli_num_rows($res_u) > 0){
+  $valid_flag = false;
+  echo "Error: Username is taken" . "<br>";
+ } else {
+  //do nothing
+ }
+   
+ 
+ //if there are no errors, then insert data
+ if ($valid_flag == true){
+  //actual SQL code to put in our DB tables
+  $sql = "INSERT INTO Homeowner (ho_fname,
+  ho_lname,ho_address,ho_email,ho_username,ho_pw_hash,
+  ho_city,ho_state,ho_zip) VALUES ('$ho_fname',
+  '$ho_lname','$ho_address','$ho_email','$ho_username',
+  '$ho_pw','$ho_city','$ho_state','$ho_zip')";
+  
+  //checks to see if query was executed/inserted
+  if(!mysqli_query($conn,$sql)){
+   //echo "Account info not inserted";
+  } else { //else if query is executed, the following runs
+   //echo "<br>";
+   //echo "Account info inserted";
+   //echo "<br>";
+   
+  }
+  mysqli_close($conn);
+  header("Location: ../index.php",
   true, 303);
-	} else {
-		echo "Data not inserted as form contains errors. Please go back, correct, and re-submit";
-		mysqli_close($conn);
-	}	
-	
+ } else {
+  echo "Data not inserted as form contains errors. Please go back, correct, and re-submit";
+  mysqli_close($conn);
+ } 
+ 
 ?>
--- scripts/update-checks.php
+++ scripts/update-checks.php
@@ -11,13 +11,23 @@
  $timestamp = date("Y-m-d H:i:s");
  $chk_comment = $_POST['comment_field'];
 
- 
  //loops through each value in array
  //and updates accordingly
  foreach($checkboxvar as $key => $value)
  {
+   /* $sql_email = "SELECT ho_email
+                 FROM Homeowner
+                 INNER JOIN Housecheck 
+                 ON Homeowner.homeowner_id = Housecheck.homeowner_id
+                 WHERE Housecheck.check_id = '$value'";
+    $result = mysqli_query($conn,$sql_email);
+    $rows = mysqli_fetch_array($result);
+    $temp_email = $rows[0];
+    echo $temp_email . "<br>"; */
+    
+    
    $new_comment = strval($chk_comment[$key]);
-   echo $new_comment . "<br>";
+   //echo $new_comment . "<br>";
    
    $sql = "UPDATE Housecheck SET chk_value = 'yes', checker_id = '$user_id', chk_at = '$timestamp', chk_comment = '$new_comment'
    WHERE check_id = '$value'";
@@ -28,9 +38,6 @@
       //do nothing
     }
  }
- 
- //close sql connection  
- mysqli_close($conn);
  
  //redirect back to admin page
  header( "Location: ../pages/adminProfile.php",
